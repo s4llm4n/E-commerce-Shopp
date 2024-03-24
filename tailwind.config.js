7b42315e8788bcc515ee5a06b555b888316e6474
s4llm4n <s4llm4n@gmail.com>
2024-03-24 22:44:28 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*html"],
+  theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+    },
+    container : {
+      center : true,
+      padding : 'rem'
+    },
+    extend: {
+      colors : {
+        primary : '#FD3D57',
+      },
+      fontFamily : {
+        'poppins' : ['Poppins'],
+        'roboto' : ['Roboto'],
+      }
+    },
+  },
+  plugins: [],
+}
+
